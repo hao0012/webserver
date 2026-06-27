02328ed8696e4ecdf65b0e75fbaa68a020fb4e0f
hao0012 <709704094@qq.com>
2026-06-27 18:06:16 +0800

    fix: run thread after loop created.

--- eventloop_thread.cpp
+++ eventloop_thread.cpp
@@ -4,13 +4,15 @@ namespace webserver {
 
 eventloop_thread::eventloop_thread(thread_init_callback user_thread_init_callback, const std::string &name): 
   name_(name),
-  thread_([this, user_thread_init_callback]() { 
+  thread_(),
+  loop_(thread_.get_id()) {
+
+  thread_ = std::thread([this, user_thread_init_callback]() {
     if (user_thread_init_callback) {
       user_thread_init_callback();
     }
-    loop_.loop(); 
-  }),
-  loop_(thread_.get_id()) {  
+    loop_.loop();
+  });
 
 }
 
